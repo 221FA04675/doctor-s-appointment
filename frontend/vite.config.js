d2e3f1af1e765d8f02ef470183ae05db16fddfd5
221FA04675 <meghanamallela6@gmail.com>
2024-11-07 21:07:48 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/doctor-s-app/',
+  base:'/Doctor-s_app/',
   server:{port:5173}
 })
